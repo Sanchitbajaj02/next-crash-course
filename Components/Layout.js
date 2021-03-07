9f990a1e3bafba6976cf964070feda1927c1b5fe
Sanchit Bajaj <sanchit02@outlook.com>
2021-03-07 22:55:17 +0530

    pages and components created

---
+++ Components/Layout.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Nav from "./Navbar";
+import Header from "./Header";
+import styles from "../styles/Layout.module.css";
+
+const Layout = ({ children }) => {
+  return (
+    <React.Fragment>
+      <Nav />
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <Header />
+          {children}
+        </main>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Layout;
